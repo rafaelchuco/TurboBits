6108a046a17587379201e2b8d57c7bd8e0475300
rafaelchuco <rafael.chuco1908@gmail.com>
2024-07-11 01:02:07 -0500

    Co-authored-by: DalexisHub <DalexisHub@users.noreply.github.com>

--- script.js
+++ script.js
@@ -158,3 +158,32 @@ function addToCart(id, price) {
   document.querySelector('.checkout').addEventListener('click', function() {
     alert('Compra confirmada');
 });
+let intro = document.querySelector('.intro');
+let logo = document.querySelector('.logo');
+let logoSpan = document.querySelectorAll('.logo-parts');
+
+window.addEventListener('DOMContentLoaded', () => {
+
+    setTimeout(() => {
+       logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+               span.classList.add('active');
+            }, (index + 1) * 100);
+       });
+
+       setTimeout(() => {
+          logoSpan.forEach((span, index) => {
+            setTimeout(() => {
+               span.classList.remove('active');
+               span.classList.add('fade'); 
+            }, (span + 1) * 50);
+          });
+       }, 2000);
+
+       setTimeout(() => {
+          intro.style.top  = '-100vh';
+       }, 2300);
+
+    });
+
+});
